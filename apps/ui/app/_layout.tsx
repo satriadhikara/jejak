846d4047ee906cae91963dbfeed19090e3300c6b
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-08-10 19:05:38 +0700

    feat: integrate gesture handling and bottom sheet provider in layout
    - Wrapped the main layout in GestureHandlerRootView for gesture support.
    - Added BottomSheetModalProvider to enable bottom sheet functionality.

--- apps/ui/app/_layout.tsx
+++ apps/ui/app/_layout.tsx
@@ -1,6 +1,15 @@
+import "react-native-gesture-handler";
 import "./global.css";
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetModalProvider>
+        <Stack screenOptions={{ headerShown: false }} />
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
+  );
 }
