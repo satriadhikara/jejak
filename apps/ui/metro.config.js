cd9ef2329374ea1309e19d0373ef832104742da5
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-08-10 19:05:24 +0700

    feat: update dependencies and configure Metro for SVG support
    - Added @gorhom/bottom-sheet and other dependencies including expo-linear-gradient and expo-location.
    - Configured Metro to handle SVG files using react-native-svg-transformer.
    - Updated nativewind-env.d.ts to declare SVG module for better type support.

--- apps/ui/metro.config.js
+++ apps/ui/metro.config.js
@@ -1,6 +1,15 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
+const { withNativeWind } = require("nativewind/metro");
 
-module.exports = withNativeWind(config, { input: './app/global.css', inlineRem: 16  })
+let config = getDefaultConfig(__dirname);
+
+config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== "svg");
+config.resolver.sourceExts.push("svg");
+
+config = withNativeWind(config, {
+  input: "./app/global.css",
+  inlineRem: 16,
+});
+
+module.exports = config;
