bc887e8580b577e6ee6af44c333cb40e5b87a8dc
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-10-10 20:32:19 +0700

    refactor(sign-in): standardize string quotes and improve UI layout
    - Updated string quotes in sign-in component for consistency.
    - Enhanced layout of sign-in UI elements for better readability and user experience.
    - Refactored authentication callback handling to maintain code clarity.

--- apps/ui/app/sign-in.tsx
+++ apps/ui/app/sign-in.tsx
@@ -1,16 +1,16 @@
-import { View, Text, Pressable, ToastAndroid } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import Feather from "@expo/vector-icons/Feather";
-import { useRef, useMemo } from "react";
-import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { signIn as signInBetterAuth } from "@/lib/auth-client";
-import { Image } from "expo-image";
-import { router } from "expo-router";
+import { View, Text, Pressable, ToastAndroid } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Feather from '@expo/vector-icons/Feather';
+import { useRef, useMemo } from 'react';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { signIn as signInBetterAuth } from '@/lib/auth-client';
+import { Image } from 'expo-image';
+import { router } from 'expo-router';
 
 export default function SignIn() {
   const bottomSheetRef = useRef<BottomSheet>(null);
-  const snapPoints = useMemo(() => ["50%"], []);
+  const snapPoints = useMemo(() => ['50%'], []);
 
   const handleOpenModal = () => {
     bottomSheetRef.current?.expand();
@@ -19,27 +19,25 @@ export default function SignIn() {
   const handleSignInWithGoogle = async () => {
     await signInBetterAuth.social(
       {
-        provider: "google",
-        callbackURL: "/",
+        provider: 'google',
+        callbackURL: '/',
       },
       {
         onError: (error) => {
           if (error.error.status === 404) {
-            ToastAndroid.show("Cant connect to backend", ToastAndroid.SHORT);
+            ToastAndroid.show('Cant connect to backend', ToastAndroid.SHORT);
           }
         },
-      },
+      }
     );
-    router.replace("/");
+    router.replace('/');
   };
 
   return (
     <GestureHandlerRootView className="flex-1">
       <View className="flex-1 bg-primary">
         <SafeAreaView className="flex flex-1 flex-col justify-end px-4 pb-5">
-          <Text className="mb-2 font-inter-regular text-5xl text-white">
-            Jejak
-          </Text>
+          <Text className="mb-2 font-inter-regular text-5xl text-white">Jejak</Text>
           <Text className="mb-4 font-inter-regular text-2xl text-secondary">
             Langkah Kecil, Perubahan Besar
           </Text>
@@ -49,8 +47,7 @@ export default function SignIn() {
           <View className="flex flex-col items-end justify-end">
             <Pressable
               onPress={handleOpenModal}
-              className="flex-row items-center gap-2 rounded-[50px] bg-white px-[22px] py-4"
-            >
+              className="flex-row items-center gap-2 rounded-[50px] bg-white px-[22px] py-4">
               <Text className="font-inter-regular text-primary">Mulai</Text>
               <Feather name="arrow-right" size={24} color="#2431AE" />
             </Pressable>
@@ -63,47 +60,37 @@ export default function SignIn() {
           index={-1}
           snapPoints={snapPoints}
           enablePanDownToClose={true}
-          backgroundStyle={{ backgroundColor: "#FFFFFF" }}
-          handleIndicatorStyle={{ backgroundColor: "#D9D9D9", width: 64 }}
-        >
+          backgroundStyle={{ backgroundColor: '#FFFFFF' }}
+          handleIndicatorStyle={{ backgroundColor: '#D9D9D9', width: 64 }}>
           <BottomSheetView className="flex-1 px-6 py-6">
             <View className="mb-6 flex flex-col items-center justify-center">
-              <Text className="font-inter-bold text-2xl text-primary">
-                Selamat datang!
-              </Text>
+              <Text className="font-inter-bold text-2xl text-primary">Selamat datang!</Text>
             </View>
 
             <View>
               <View className="mb-6 flex-row items-center gap-4">
                 <View className="h-[1px] flex-1 bg-[#ABAFB5]" />
-                <Text className="font-inter-regular text-[#ABAFB5]">
-                  atau masuk dengan
-                </Text>
+                <Text className="font-inter-regular text-[#ABAFB5]">atau masuk dengan</Text>
                 <View className="h-[1px] flex-1 bg-[#ABAFB5]" />
               </View>
 
               <View className="flex flex-row items-center justify-center gap-3">
                 <Pressable
                   className="w-1/2 flex-row items-center justify-center gap-2 rounded-2xl border border-[#ABAFB5] bg-white px-6 py-4"
-                  onPress={handleSignInWithGoogle}
-                >
+                  onPress={handleSignInWithGoogle}>
                   <Image
-                    source={require("@/assets/icons/google.svg")}
+                    source={require('@/assets/icons/google.svg')}
                     style={{ width: 19, height: 19 }}
                   />
-                  <Text className="font-inter-semi-bold text-[#4B4D53]">
-                    Google
-                  </Text>
+                  <Text className="font-inter-semi-bold text-[#4B4D53]">Google</Text>
                 </Pressable>
 
                 <Pressable className="w-1/2 flex-row items-center justify-center gap-3 rounded-2xl border border-[#ABAFB5] bg-white px-6 py-4">
                   <Image
-                    source={require("@/assets/icons/apple.svg")}
+                    source={require('@/assets/icons/apple.svg')}
                     style={{ width: 19, height: 19 }}
                   />
-                  <Text className="font-inter-semi-bold text-base text-[#4B4D53]">
-                    Apple ID
-                  </Text>
+                  <Text className="font-inter-semi-bold text-base text-[#4B4D53]">Apple ID</Text>
                 </Pressable>
               </View>
             </View>
--- apps/ui/lib/auth-client.ts
+++ apps/ui/lib/auth-client.ts
@@ -1,13 +1,13 @@
-import { createAuthClient } from "better-auth/react";
-import { expoClient } from "@better-auth/expo/client";
-import * as SecureStore from "expo-secure-store";
+import { createAuthClient } from 'better-auth/react';
+import { expoClient } from '@better-auth/expo/client';
+import * as SecureStore from 'expo-secure-store';
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
   plugins: [
     expoClient({
-      scheme: "jejak",
-      storagePrefix: "jejak",
+      scheme: 'jejak',
+      storagePrefix: 'jejak',
       storage: SecureStore,
     }),
   ],
