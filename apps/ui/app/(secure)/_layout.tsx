fb04557c36d0568cad9e3b8bf3779dd582e478df
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-10-10 20:31:33 +0700

    feat: update authentication and UI components
    - Upgraded better-auth to version 1.3.27 and integrated Google sign-in functionality.
    - Enhanced environment configuration with new Google client ID and secret.
    - Updated UI components to support new authentication methods and improved user experience.
    - Added new icons for Google and Apple sign-in options.
    - Refactored auth client to utilize expo secure storage for session management.
    - Improved layout handling for authenticated routes with loading and error states.

--- apps/ui/app/(secure)/_layout.tsx
+++ apps/ui/app/(secure)/_layout.tsx
@@ -1,5 +1,29 @@
-import { Stack } from 'expo-router';
+import { Redirect, Stack } from 'expo-router';
+import { useSession } from '@/lib/auth-client';
+import { ActivityIndicator, View, Text } from 'react-native';
 
 export default function Layout() {
+  const { data: session, isPending, error } = useSession();
+
+  if (isPending) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" color="#2431AE" />
+      </View>
+    );
+  }
+
+  if (error) {
+    console.log(error);
+    return (
+      <View className="flex-1 items-center justify-center">
+        <Text>Error</Text>
+      </View>
+    );
+  }
+
+  if (!session) {
+    return <Redirect href="/sign-in" />;
+  }
   return <Stack screenOptions={{ headerShown: false }} />;
 }
