56f86ac08c295f562d716bcddeff045b510a3ccd
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-10-11 20:09:03 +0700

    feat: add new components for report history and user interaction
    - Introduced EmptyReportHistory component to display a message when no reports are available.
    - Added PointsCard component to show user points and provide a button to create a report.
    - Created RankingCard component to display user rankings with avatar and points.
    - Implemented ReportCard component to present individual report details with status and action button.

---
+++ apps/ui/components/beranda/points-card.tsx
@@ -0,0 +1,39 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Card } from 'react-native-paper';
+import { Ionicons } from '@expo/vector-icons';
+import { Skeleton } from '../Skeleton';
+
+interface PointsCardProps {
+  isPending: boolean;
+  points: number;
+  onCreateReport: () => void;
+}
+
+const PointsCard = ({ isPending, points, onCreateReport }: PointsCardProps) => {
+  return (
+    <Card className="mt-2.5 rounded-2xl">
+      <Card.Content className="flex-row items-center justify-between rounded-2xl bg-white">
+        <View>
+          <Text className="font-inter-medium text-sm text-[#242528]">Poin Jejak</Text>
+          <View className="mt-1 flex-row items-center">
+            <Ionicons name="footsteps-outline" size={20} color="#00D996" />
+            {isPending ? (
+              <Skeleton className="ml-1.5 text-lg font-bold text-[#242528]" width={50} />
+            ) : (
+              <Text className="ml-1.5 font-inter-medium text-2xl text-[#242528]">
+                {points} poin
+              </Text>
+            )}
+          </View>
+        </View>
+        <TouchableOpacity
+          className="items-center justify-center rounded-[50px] bg-[#EBF4FF] px-[18px] py-2.5"
+          onPress={onCreateReport}>
+          <Text className="font-inter-semi-bold text-sm text-[#2431AE]">Buat Laporan</Text>
+        </TouchableOpacity>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default PointsCard;
