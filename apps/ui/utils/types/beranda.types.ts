ab8693160edb3d4691a3ee9afd621710ff9e0f66
satriadhikara <84092642+satriadhikara@users.noreply.github.com>
2025-10-11 20:09:42 +0700

    feat: add API utilities and types for user points and rankings
    - Created beranda.api.ts to implement API calls for fetching user points and top users by points.
    - Introduced beranda.types.ts to define TypeScript interfaces for user points and top users responses.

---
+++ apps/ui/utils/types/beranda.types.ts
@@ -0,0 +1,34 @@
+export interface UserPointsResponse {
+  data: {
+    points: number;
+    rank: number;
+  };
+}
+
+export interface TopUsersByPointsResponse {
+  data: {
+    id: string;
+    name: string;
+    image: string;
+    points: number;
+  }[];
+}
+
+export interface TopUsersByPointsItem {
+  id: string;
+  rank: number;
+  name: string;
+  avatar: string;
+  points: number;
+  isCurrentUser: boolean;
+}
+
+export interface ReportHistoryItem {
+  id: number;
+  title: string;
+  date: string;
+  location: string;
+  status: string;
+  statusColor: string;
+  statusBgColor: string;
+}
